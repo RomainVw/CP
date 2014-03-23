2b3035243186610d4aebbf4c908aec0dfe2453ea
Romain <romain.vanwelde@student.uclouvain.be>
2014-03-23 21:00:07 +0100

    Report + tests

--- Projet3/code/script_Q2_3_3.sh
+++ Projet3/code/script_Q2_3_3.sh
@@ -4,14 +4,14 @@ chaineX=knapsack_instances/A/knapsack-A-
 
 for i in $(seq 1 1 25);
 do
-    /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineX$i.txt > LBUB/A/LBUB-A-f$i.txt
+    /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineX$i.txt >> LBUB/A/LBUB-A.txt
 done
 
 chaineY=knapsack_instances/B/knapsack-B-
 
 for i in $(seq 1 1 25);
 do
-/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineY$i.txt > LBUB/B/LBUB-B-f$i.txt
+/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineY$i.txt >> LBUB/B/LBUB-B.txt
 done
 
 
@@ -19,5 +19,5 @@ chaineZ=knapsack_instances/C/knapsack-C-
 
 for i in $(seq 1 1 25);
 do
-/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineZ$i.txt > LBUB/C/LBUB-C-f$i.txt
+/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineZ$i.txt >> LBUB/C/LBUB-C.txt
 done
