3ecfa4b46c1d73de749c2a05a2953ecd3d060d4a
Romain <romain.vanwelde@student.uclouvain.be>
2014-03-23 17:07:42 +0100

    Finished code (maybe have a look at the airport problem)

---
+++ Projet3/code/script_Q2_3_3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+chaineX=knapsack_instances/A/knapsack-A-
+
+for i in $(seq 1 1 25);
+do
+    /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineX$i.txt > LBUB/A/LBUB-A-f$i.txt
+done
+
+chaineY=knapsack_instances/B/knapsack-B-
+
+for i in $(seq 1 1 25);
+do
+/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineY$i.txt > LBUB/B/LBUB-B-f$i.txt
+done
+
+
+chaineZ=knapsack_instances/C/knapsack-C-
+
+for i in $(seq 1 1 25);
+do
+/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineZ$i.txt > LBUB/C/LBUB-C-f$i.txt
+done
