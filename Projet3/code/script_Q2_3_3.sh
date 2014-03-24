a381a5741424be700a3d043a1db62879fe0b7e40
Martin Crochelet <martin.crochelet@gmail.com>
2014-03-24 11:25:02 +0100

    script 3.3

--- Projet3/code/script_Q2_3_3.sh
+++ Projet3/code/script_Q2_3_3.sh
@@ -4,14 +4,18 @@ chaineX=knapsack_instances/A/knapsack-A-
 
 for i in $(seq 1 1 25);
 do
+    echo "\begin{figure}[!h]\begin{tabular}{|r|r|r|r|r|}\hline" >> LBUB/A/LBUB-A.txt
     /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineX$i.txt >> LBUB/A/LBUB-A.txt
+    echo "\end{tabular}\end{figure}" >> LBUB/A/LBUB-A.txt
 done
 
 chaineY=knapsack_instances/B/knapsack-B-
 
 for i in $(seq 1 1 25);
 do
-/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineY$i.txt >> LBUB/B/LBUB-B.txt
+    echo "\begin{figure}[!h]\begin{tabular}{|r|r|r|r|r|}\hline" >> LBUB/B/LBUB-B.txt
+    /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineY$i.txt >> LBUB/B/LBUB-B.txt
+    echo "\end{tabular}\end{figure}" >> LBUB/B/LBUB-B.txt
 done
 
 
@@ -19,5 +23,7 @@ chaineZ=knapsack_instances/C/knapsack-C-
 
 for i in $(seq 1 1 25);
 do
-/Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineZ$i.txt >> LBUB/C/LBUB-C.txt
+    echo "\begin{figure}[!h]\begin{tabular}{|r|r|r|r|r|}\hline" >> LBUB/C/LBUB-C.txt
+    /Applications/Comet/comet -j2 knapsackUB_LB.co -f$chaineZ$i.txt >> LBUB/C/LBUB-C.txt
+    echo "\end{tabular}\end{figure}" >> LBUB/C/LBUB-C.txt
 done
